9702c325f3661c2f1ba923c2f6f5c8719e76ad11
那他 <1696674719@qq.com>
2017-07-29 17:29:01 +0800

    高端升级v1.4

--- majors/Plugin.php
+++ majors/Plugin.php
@@ -4,7 +4,6 @@
  * 
  * @package majors 
  * @author 权那他
- * @version 1.0.0
  * @link https://github.com/kraity/Major
  */
 class majors_Plugin implements Typecho_Plugin_Interface
@@ -18,23 +17,17 @@ class majors_Plugin implements Typecho_Plugin_Interface
      */
     public static function activate()
     {
-        $db = Typecho_Db::get();
-        $prefix = $db->getPrefix();
-
-        // contents 表中若无 format 字段则添加
-        if (!array_key_exists('format', $db->fetchRow($db->select()->from('table.contents'))))
-            $db->query("ALTER TABLE `".$prefix."contents` ADD `format` varchar(16) DEFAULT 'post'");
+        $msg = majors_Plugin::install();
 			
 		//添加文章
         Typecho_Plugin::factory('admin/write-post.php')->option = array('majors_Plugin', 'formatsSelect');       
 		
 		//编辑文章
-		Typecho_Plugin::factory('Widget_Contents_Post_Edit')->write = array('majors_Plugin', 'formatsSet');    
-        
-        
+		Typecho_Plugin::factory('Widget_Contents_Post_Edit')->write = array('majors_Plugin', 'formatsSet');
         Typecho_Plugin::factory('index.php')->begin = array('majors_Plugin', 'Widget_index_begin');
+        Typecho_Plugin::factory('admin/footer.php')->end = array('majors_Plugin', 'Footeradmin');
         Typecho_Plugin::factory('Widget_Archive')->indexHandle = array('majors_Plugin', 'sticky');
-
+        return _t($msg);
     }
 
     /**
@@ -45,7 +38,15 @@ class majors_Plugin implements Typecho_Plugin_Interface
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate(){}
+    public static function deactivate(){
+        $config = Typecho_Widget::widget('Widget_Options')->plugin('majors');
+        $delmat = $config->delmat;
+        if ($delmat == 0) {
+            $db = Typecho_Db::get();
+            $prefix = $db->getPrefix();
+            $db->query("ALTER TABLE `".$prefix."contents` DROP COLUMN `format`");
+        }
+    }
     
     /**
      * 获取插件配置面板
@@ -55,28 +56,22 @@ class majors_Plugin implements Typecho_Plugin_Interface
      * @return void
      */
     public static function config(Typecho_Widget_Helper_Form $form){
-        $compression_open = new Typecho_Widget_Helper_Form_Element_Checkbox('compression_open', array('compression_open' => '开启gzip'), null, _t('是否开启gzip'));
-        $form->addInput($compression_open);
 
-        $compression_level = new Typecho_Widget_Helper_Form_Element_Select('compression_level', array('1' => '1', '2' => '2', '3' => '3', '4' => '4', '5' => '5', '6' => '6', '7' => '7', '8' => '8', '9' => '9'), '5', _t('gzip压缩级别:'), _t('这个参数值范围是0-9，0表示无压缩，9表示最大压缩，当然压缩程度越高越费CPU。*推荐：5'));
-        $form->addInput($compression_level);
+        $delmat = new Typecho_Widget_Helper_Form_Element_Radio(
+            'delmat', array(
+            '0' => '删除',
+            '1' => '不删除',
+        ), '1', '删除数据表:', '请选择是否在禁用插件时，format字段');
+        $form->addInput($delmat);
 
         $compress_html = new Typecho_Widget_Helper_Form_Element_Checkbox('compress_html', array('compress_html' => '开启压缩HTML'), null, _t('是否开启压缩HTML'), _t('当开启后页面与原来页面不一致时请关闭'));
         $form->addInput($compress_html);
-
-        $keyword_replace = new Typecho_Widget_Helper_Form_Element_Textarea('keyword_replace', null, null, _t('HTML关键词替换'), _t('作用：主要把附件的内容转到七牛。一行一个。格式：关键词=替换关键词'));
-        $form->addInput($keyword_replace);
         
         $sticky_cids = new Typecho_Widget_Helper_Form_Element_Text(
           'sticky_cids', NULL, '',
           '置顶文章的 cid', '按照排序输入, 请以半角逗号或空格分隔 cid.');
         $form->addInput($sticky_cids);
 
-        $sticky_html = new Typecho_Widget_Helper_Form_Element_Textarea(
-          'sticky_html', NULL, "<strong>[置顶]</strong>",
-          '置顶标题的 html', '');
-        $sticky_html->input->setAttribute('rows', '7')->setAttribute('cols', '80');
-        $form->addInput($sticky_html);
 		$formats = new Typecho_Widget_Helper_Form_Element_Checkbox('format', array(
             'aside'=>'日志', 
             'gallery'=>'相册', 
@@ -97,6 +92,25 @@ class majors_Plugin implements Typecho_Plugin_Interface
      * @return void
      */
     public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+
+    public static function install(){
+
+        $configLink = '<a href="' . Helper::options()->adminUrl . 'options-plugin.php?config=majors' . '">请设置</a>';
+
+        $db = Typecho_Db::get();
+        $prefix = $db->getPrefix();
+
+        // contents 表中若无 format 字段则添加
+        if (!array_key_exists('format', $db->fetchRow($db->select()->from('table.contents')))) {
+            $db->query("ALTER TABLE `".$prefix."contents` ADD `format` varchar(16) DEFAULT 'post'");
+
+            return '成功创建 format 字段，插件启用成功，' . $configLink;
+        } else {
+            return 'format 字段已存在，插件启用成功，' . $configLink;
+        }
+    }
+
 
     /**
      * 编辑文章页, 添加 "文章形式" 选项
@@ -225,71 +239,27 @@ class majors_Plugin implements Typecho_Plugin_Interface
         $row = $db->fetchRow($db->select('format')->from('table.contents')->where('cid = ?', $cid));
 		return $row['format'];
 	}
-    
-    
-    public static function Widget_index_begin() {
-        ob_start('majors_Plugin::qlwz_ob_handler');
-    }
 
-    public static function Widget_Archive_beforeRender() {
-        ob_start('majors_Plugin::qlwz_ob_handler');
-    }
 
-    public static function qlwz_ob_handler($buffer) {
-        $settings = Helper::options()->plugin('majors');
-
-        if ($settings->keyword_replace) {
-            $list = explode("\r\n", $settings->keyword_replace);
-            foreach ($list as $tmp) {
-                list($old, $new) = explode('=', $tmp);
-                $buffer = str_replace($old, $new, $buffer);
-            }
-        }
-
-        if ($settings->compress_html) {
-            $buffer = self::qlwz_compress_html($buffer);
-        }
-
-        if ($settings->compression_open) {
-            $buffer = self::ob_gzip($buffer, $settings->compression_level);
-        } else {
-            if (ini_get('zlib.output_compression')) {
-                ini_set('zlib.output_compression', 'Off');
-            }
-        }
-        return $buffer;
-    }
-
-    public static function ob_gzip($buffer, $level) {
-        if (ini_get('zlib.output_compression')) {
-            if (ini_get('zlib.output_compression_level') != $level) {
-                ini_set('zlib.output_compression_level', $level);
-            }
-            return $buffer;
-        }
-        if (headers_sent() || !extension_loaded('zlib') || !strstr($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip')) {
-            return $buffer;
-        }
-
-        $out_buffer = gzencode($buffer, $level);
-        if (strlen($out_buffer) < strlen($buffer)) {
-            header('Content-Encoding: gzip');
-            header('Vary: Accept-Encoding');
-            header('Content-Length: ' . strlen($out_buffer));
-        } else {
-            $out_buffer = $buffer;
-        }
-        return $out_buffer;
-    }
 
     /**
      * 压缩HTML代码
      *
      * @author 情留メ蚊子 <qlwz@qq.com>
-     * @version 1.0.0.0 By 2016-11-23
      * @param string $html_source HTML源码
      * @return string 压缩后的代码
      */
+    public static function Widget_index_begin() {
+        ob_start('majors_Plugin::qlwz_ob_handler');
+    }
+    public static function qlwz_ob_handler($buffer) {
+        $settings = Helper::options()->plugin('majors');
+        if ($settings->compress_html) {
+            $buffer = self::qlwz_compress_html($buffer);
+        }
+
+        return $buffer;
+    }
     public static function qlwz_compress_html($html_source) {
         $chunks = preg_split('/(<!--<nocompress>-->.*?<!--<\/nocompress>-->|<nocompress>.*?<\/nocompress>|<pre.*?\/pre>|<textarea.*?\/textarea>|<script.*?\/script>)/msi', $html_source, -1, PREG_SPLIT_DELIM_CAPTURE);
         $compress = '';
@@ -355,7 +325,7 @@ class majors_Plugin implements Typecho_Plugin_Interface
 
         $db = Typecho_Db::get();
         $paded = $archive->request->get('page', 1);
-        $sticky_html = $config->sticky_html ? $config->sticky_html : "<strong>[置顶]</strong>";
+        $sticky_html = "<strong>[置顶]</strong>";
 
         foreach($sticky_cids as $cid) {
           if ($cid && $sticky_post = $db->fetchRow($archive->select()->where('cid = ?', $cid))) {
@@ -365,6 +335,31 @@ class majors_Plugin implements Typecho_Plugin_Interface
               }
               $select->where('table.contents.cid != ?', $cid); // 使文章不重覆
           }
+        }
+    }
+    
+    
+    /**
+     * 后台参数
+     *
+     * @author 那他
+     */
+    public static function Footeradmin()
+    {
+        
+        $url = $_SERVER['PHP_SELF'];
+        $filename = substr($url, strrpos($url, '/') + 1);
+        if ($filename == 'index.php') {
+            echo '<script>
+$(document).ready(function() {
+  $("#start-link").append("<li><a href=\"./options-theme.php\">外观变量</a></li><li><a href=\"https://home.console.aliyun.com/\" target=\"_blank\">阿里云控制台</a></li><li><a href=\"http://console.krait.cn:8888\" target=\"_blank\">服务器控制台</a></li>");
+});
+</script>';
+        }
+        
+        if ($filename == 'theme-editor.php') {
+            echo '<style>.submit{display:none}</style>';
+            
         }
     }
 }
