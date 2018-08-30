6bec64f73f0a06ceee344d4139d1f2ed420439fd
那他 <kraits@qq.com>
2018-08-31 01:47:50 +0800

    Major 2.0  补丁08.31

--- Major/res/showfoot.php
+++ Major/res/showfoot.php
@@ -1,33 +1,52 @@
-<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit;
-/**
- *
- * <div class="post-footer">
-<div class="post-infoBox">
-<li>
-<a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author" class="intro-author"><?php $this->author(); ?></a>
-</li>
-<li>
-<?php majors_Plugin::theViews(); ?> Views
-</li>
-<li>
-<time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time>
-</li>
-<li>
-<?php echo Major::artCount($this->cid); ?>个字
-</li>
-<li>
-<?php $this->category(','); ?><?php $this->tags(',', true, '无标签'); ?>
-</li>
-</div>
-</div>
- */
-?>
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 
-<div class="show-foot">
-    <div class="copyright" data-toggle="tooltip" data-html="true" data-original-title="转载请联系作者获得授权，并注明转载地址"><span>© 著作权归作者所有</span>
+<div class="major-post-foot">
+    <div id="show-foot-tab1">
+        <div class="show-foot">
+            <ul class="mdui-list mdui-list-dense">
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">copyright</i>
+                    <div class="mdui-list-item-content">著作权归作者所有</div>
+                </li>
+            </ul>
+        </div>
     </div>
-    <div class="notebook">
-        <span>最后修改：<?php echo date('Y 年 m 月 d 日', $this->modified);?></span>
+    <div id="show-foot-tab2">
+        <div class="show-foot-info">
+
+            <ul class="mdui-list mdui-list-dense">
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">person</i>
+                    <div class="mdui-list-item-content"><a itemprop="name" href="<?php $this->author->permalink(); ?>" rel="author" class="intro-author"><?php $this->author(); ?></a></div>
+                </li>
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">play_arrow</i>
+                    <div class="mdui-list-item-content"><?php majors_Plugin::theViews(); ?> Views</div>
+                </li>
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">create</i>
+                    <div class="mdui-list-item-content"><time datetime="<?php $this->date('c'); ?>" itemprop="datePublished"><?php $this->date(); ?></time></div>
+                </li>
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">autorenew</i>
+                    <div class="mdui-list-item-content">最后修改：<?php echo date('Y 年 m 月 d 日', $this->modified);?></div>
+                </li>
+                <li class="mdui-list-item mdui-ripple">
+                    <i class="mdui-list-item-icon mdui-icon material-icons">local_offer</i>
+                    <div class="mdui-list-item-content listCategoryTags"><?php $this->category(''); ?><?php $this->tags('', true, '无标签'); ?></div>
+                </li>
+            </ul>
+        </div>
     </div>
+    <div class="mdui-tab" id="foot-post-tab">
+        <a href="#show-foot-tab1" class="mdui-ripple"><i class="mdui-icon material-icons">copyright</i></a>
+        <a href="#show-foot-tab2" class="mdui-ripple"><i class="mdui-icon material-icons">info_outline</i></a>
+    </div>
+    <script type="text/javascript">
+        var inst = new mdui.Tab('#foot-post-tab');
+    </script>
 </div>
+
+
+
 
