b81dc433f6717637c4861f0759cd880434f9ea36
那他 <kraits@qq.com>
2018-10-03 18:29:14 +0800

    Major 2.1

--- Major/link.php
+++ Major/link.php
@@ -12,7 +12,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
     <div class="articles-post post-header">
         <div class="post-head">
             <div class="back">
-                <button onclick="backClick()" class="mdui-btn mdui-btn-icon mdui-ripple"><i class="mdui-icon material-icons">arrow_back</i></button>
+                <button onclick="historyBack();" class="mdui-btn mdui-btn-icon mdui-ripple"><i class="mdui-icon material-icons">arrow_back</i></button>
             </div>
             <div class="container">
                 <div class="title">
@@ -33,12 +33,24 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
     <div class="links" id="main" role="main">
         <div class="container">
             <article class="major-article content-wrap" itemscope itemtype="http://schema.org/BlogPosting">
-                <div class="post-content major-text">
+                <div class="post-content">
                     <?php $str = preg_replace('#<li>(.*?)</li>#','<li>$1</li>', $this->content);
-                    $str = preg_replace('#<li>(.*?)<a href="(.*?)">(.*?)</a></li>#','<a class="mdui-ripple" href="$2" target="_blank">$1<p>$3</p></a>',$str);
-                    $str = preg_replace('#<ul>#','<div class="link-group">', $str);
-                    $str = preg_replace('#</ul>#','</div>', $str);
-                    $str = preg_replace('#\@\((.*?)\)#','<img src="">',$str);
+                    $str = preg_replace('#<li><img src="(.*?)" alt="(.*?)" title="(.*?)">(.*?)<a href="(.*?)">(.*?)</a></li>#','<div class="post-list-item col-sm-6 col-xs-12">
+                        <div class="post-list-item-container">
+                            <div class="item-label">
+                                <div class="item-title">
+                                    <a href="$5" target="_blank">$6</a>
+                                </div>
+                                <div class="item-meta clearfix">
+                                    <div class="item-meta-ico bg-ico-book" style="background: url($1) no-repeat;background-size: 40px auto;"></div>
+                                    <div class="item-meta-cat"> <a href="$5" target="_blank">$3</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>',$str);
+                    $str = preg_replace('#<ul>#','<div class="post-lists-body">', $str);
+                    $str = preg_replace('#</ul>#','</div><div class="clear"></div>', $str);
                     echo $str;?>
 
                 </div>
@@ -46,15 +58,6 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
             </article>
         </div>
     </div>
-    <script>
-        function backClick(){
-            if(history.length<1){
-                window.location.href = "<?php $this->options->siteUrl(); ?>";
-            }else{
-                window.history.back();
-            }
-        }
-    </script>
     <div class="comment-here">
         <div class="container">
             <?php $this->need('comments.php'); ?>
