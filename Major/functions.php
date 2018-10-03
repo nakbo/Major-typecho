40ea0151976cd14efec9e8e513784f4876041fee
那他 <kraits@qq.com>
2018-10-03 18:27:59 +0800

    Major 2.1

--- Major/functions.php
+++ Major/functions.php
@@ -9,9 +9,155 @@ require_once('lib/Major.php');
  *
  *
  */
-function themeConfig($form) {
+function themeConfig($form) { ?>
+    <div id="majorModel" class="majorModel"></div>
+    <script type="text/javascript" src="./js/jquery.js"></script>
+    <script>
+        function formatDateTime(nS) {
+            return new Date(parseInt(nS) * 1000).toLocaleString().substr(0,12)
+        }
+        var infoMajor = <?php echo file_get_contents(Major::$api['infoMajor']);?>;
+       var infoModel = "<div class=\"majorPv\">你正在使用",
+            date = infoMajor.Major[0].d,
+            ver = infoMajor.Major[0].v,
+            updetaTime = "<?php echo Major::$Major['updateTime']; ?>",
+            versionNow = "<?php echo Major::$Major['version']; ?>",
+            dateNow = formatDateTime(date),
+            dataUpGit = "<?php echo Major::$Major['github']; ?>";
 
-    Major::getVersion();
+       if(date>updetaTime){
+           infoModel += '<strong>'+versionNow+'</strong> 版本，'+dateNow+'更新最新版本为 <strong style="color:#000000;">'+ver+'</strong><a href="'+dataUpGit+'"><button type="submit" class="btn btn-warn">前往更新</button></a>';
+       }else{
+           infoModel += '最新版 '+ver;
+       }
+        infoModel +="</div>";
+       $("#majorModel").html(infoModel);
+    </script>
+    <script>
+        function backData() {
+            var newLocal= localStorage.backupData;
+            if (newLocal!= "" && newLocal!= undefined) {
+                var jsonData = JSON.parse(newLocal);
+                var data = jsonData;
+                for(var i in data){
+                    switch (data[i].type) {
+                        case "text":
+                            $('#'+document.getElementsByName(data[i].name)[0].id).val(data[i].value);
+                            break;
+                        case "textarea":
+                            $('#'+document.getElementsByName(data[i].name)[0].id).val(data[i].val);
+                            break;
+                        case "checkbox":
+                            switch (data[i].checked) {
+                                case 'true':
+                                    document.getElementById(data[i].id).checked = true;
+                                    break;
+                                case 'false':
+                                    document.getElementById(data[i].id).checked = false;
+                                    break;
+                            }
+                            break;
+
+                        case "radio":
+                            switch (data[i].checked) {
+                                case 'true':
+                                    $('#'+data[i].id).prop("checked","checked");
+                                    break;
+                                case 'false':
+                                    $('#'+data[i].id).removeAttr("checked");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        function backupData() {
+            var json = [];
+            var inputs = $("input");
+            var textareas = $("textarea");
+            for (i = 0, len = inputs.length; i < len; i++) {
+                var j = {};
+                var input = inputs[i];
+                j.name = input.name;
+                j.type = input.type;
+                j.id = input.id;
+                switch (input.type){
+                    case "text":
+                        j.value = input.value;
+                        break;
+
+                    case "radio":
+                        j.checked = ''+input.checked+'';
+                        break;
+
+                    case "checkbox":
+                        j.checked = ''+input.checked+'';
+                        break;
+                    default:
+                }
+                json.push(j);
+            }
+            for (i = 0, len = textareas.length; i < len; i++) {
+                var j = {};
+                var textarea = textareas[i];
+                j.name = textarea.name;
+                j.type = 'textarea';
+                j.id = textarea.id;
+                j.val = $('#'+textarea.id).val();
+
+                json.push(j);
+            }
+            var dataUp = JSON.stringify(json);
+            localStorage.backupData = dataUp;
+        }
+        jQuery(function(){
+            $(".typecho-option-submit").append("<li><button type=\"button\" class=\"btn primary\" onclick=\"backupData()\">备份</button></li><li><button type=\"button\" class=\"btn primary\" onclick=\"backData()\">恢复</button></li>");
+        });
+    </script>
+    <script type="text/javascript">
+        $(function() {
+            var Gra1 = $("#useGravatar-ma"),
+                Gra0 = $("#useGravatar-gr");
+            if(!Gra1.is(":checked")) {
+                var Grat = $("#typecho-option-item-masterImgUrl-1");
+                Grat.attr("style","color:#999")
+                    .find("input").attr("disabled","disabled");
+                Gra1.click(function() {
+                    Grat.removeAttr("style")
+                        .find("input").removeAttr("disabled");
+                });
+                Gra0.click(function() {
+                    Grat.attr("style","color:#999")
+                        .find("input").attr("disabled","disabled");
+                });
+            }
+        });
+    </script>
+    <style>
+        .majorModel{
+            font-family:'Microsoft Yahei', sans-serif
+        }
+        .majorModel a {
+            color:black;
+        }
+        .majorModel .btn {
+            padding: 0 7px;
+            height: 20px;
+            margin-left: 10px;
+            font-size: 10px;
+            margin-top: -7px;
+        }
+        .typecho-option-submit{
+            position: fixed !important;
+            bottom: 10px !important;
+            right: 30px !important;
+        }
+        .typecho-option-submit li{
+            display: inline;
+            margin-right: 5px;
+        }
+    </style>
+    <?php
 
     $faviconUrl = new Typecho_Widget_Helper_Form_Element_Text('faviconUrl', NULL, '/favicon.ico', _t('Favicon.ico'), _t('此处填入favicon.ico地址'));
     $form->addInput($faviconUrl);
@@ -22,7 +168,7 @@ function themeConfig($form) {
         ),
         'gr', _t('引用头像方式'), _t('默认启用公认头像，公认头像是全球公认头像(Gravatar),使用的邮箱是管理员的邮箱.自定头像是你自己自定义的头像,在这里的下方设置即可.'));
     $form->addInput($useGravatar);
-    
+
     $masterImgUrl = new Typecho_Widget_Helper_Form_Element_Text('masterImgUrl', NULL, 'https://secure.gravatar.com/avatar/4e4559eceb7fbd4bca7925710592b1b9?s=70&r=G&d=mm', _t('自定头像'), _t('此处填入头像地址,用于mat头部显示,文章页时显示作者头像 '));
     $form->addInput($masterImgUrl);
 
@@ -71,7 +217,16 @@ function themeConfig($form) {
         'https://gravatar.loli.net/avatar', _t('Gravatar选择服务器'), _t('替换Gravatar头像服务器') );
     $form->addInput($serverGravatar->multiMode());
 
-    $stickyCid = new Typecho_Widget_Helper_Form_Element_Text('stickyCid', NULL, '1739,1671,1628', _t('置顶文章的 cid'), _t('按照排序输入, 请以半角逗号或空格分隔 cid.'));
+    /** libCdnjs **/
+    $libCdnjs = new Typecho_Widget_Helper_Form_Element_Radio( 'libCdnjs',  array(
+        'https://cdnjs.loli.net/ajax/libs/'        =>  'cdnjs.loli 公共库 ( https://cdnjs.loli.net/ajax/libs/ )',
+        'https://lib.baomitu.com/'        =>  'cdn.baomitu 公共库 ( https://lib.baomitu.com/ )',
+        'https://cdnjs.cloudflare.com/ajax/libs/'        =>  'cdnjs.cloudflare 公共库 ( https://cdnjs.cloudflare.com/ajax/libs/ )',
+        'https://cdn.bootcss.com/'        =>  'cdn.bootcss 公共库 ( https://cdn.bootcss.com/ )'),
+        'https://cdnjs.loli.net/ajax/libs/', _t('选择公共库'), _t('替换选择需要的公共库') );
+    $form->addInput($libCdnjs->multiMode());
+
+    $stickyCid = new Typecho_Widget_Helper_Form_Element_Text('stickyCid', NULL, '1739,1738,1671,1628', _t('置顶文章的 cid'), _t('按照排序输入, 请以半角逗号或空格分隔 cid.'));
     $form->addInput($stickyCid);
 
     $rewardJson = new Typecho_Widget_Helper_Form_Element_Textarea('rewardJson', NULL, '{ reward: [{ name: "支付宝" }, { img: "https://wx4.sinaimg.cn/large/006U7bU2gy1fl6dogepplj30u00u0gps.jpg"}] },{ reward: [{ name: "微信" }, { img: "https://ws3.sinaimg.cn/large/006U7bU2gy1furtrmqw95j30w00w0q6u.jpg"}] },{ reward: [{ name: "QQ" }, { img: "https://ws3.sinaimg.cn/large/006U7bU2gy1fus05omzozj30k00u0abg.jpg"}] }', _t('打赏Json'), _t('此处填入打赏的Json。'));
@@ -80,7 +235,7 @@ function themeConfig($form) {
     $socialJsonUrl = new Typecho_Widget_Helper_Form_Element_Text('socialJsonUrl', NULL, '//at.alicdn.com/t/font_569951_ou3gfpi8iyp.css', _t('社交Json Font class'), _t('此处填入阿里巴巴矢量图标库中你的项目中Font class的在线链接.'));
     $form->addInput($socialJsonUrl);
 
-    $socialJson = new Typecho_Widget_Helper_Form_Element_Textarea('socialJson', NULL, '{s:"aliyun",u:"https://code.aliyun.com/Krait"},{s:"github",u:"https://github.com/kraity"},{s:"mayun",u:"https://gitee.com/kraity"},{s:"weibo",u:"https://weibo.com/Kraity"},{s:"qq",u:"javascript:;",content:"QQ 1696674719"},{s:"weixin",u:"javascript:;",content:"微信号 Kraity"},{s:"mail",u:"javascript:;",content:"邮箱 kraits@qq.com"}', _t('社交Json'), _t('此处填入社交的Json。'));
+    $socialJson = new Typecho_Widget_Helper_Form_Element_Textarea('socialJson', NULL, '{s:"github",u:"https://github.com/kraity"},{s:"mayun",u:"https://gitee.com/kraity"},{s:"weibo",u:"https://weibo.com/Kraity"},{s:"qq",u:"javascript:;",content:"QQ 1696674719"},{s:"weixin",u:"javascript:;",content:"微信号 Kraity"},{s:"mail",u:"javascript:;",content:"邮箱 kraits@qq.com"}', _t('社交Json'), _t('此处填入社交的Json。'));
     $form->addInput($socialJson);
 
     $bloggerGx = new Typecho_Widget_Helper_Form_Element_Text('bloggerGx', NULL, '期待已久的“创造”之旅即将开始', _t('首页描述说明'), _t('此处填入首页描述说明,它用于在首页描述说明。'));
@@ -96,7 +251,7 @@ function themeConfig($form) {
         array(
             'usePangu' => _t('文章里中英文间加空格'),
             'usePostContentImg' => _t('文章里中加入文章缩略图在其中')
-            ),
+        ),
         array('usePangu','usePostContentImg'), _t('其他开关'));
     $form->addInput($useBlock->multiMode());
 
