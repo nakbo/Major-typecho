c7fb2b01620499e8cf7b32c29395376a8a4356f2
那他 <kraits@qq.com>
2019-02-16 11:20:45 +0800

    Add files via upload

--- majors/Plugin.php
+++ majors/Plugin.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * Krait Major 主题集成插件
- * 
- * @package majors 
+ *
+ * @package majors
  * @author 权那他
  * @link https://github.com/kraity/Major
  */
@@ -10,7 +10,7 @@ class majors_Plugin implements Typecho_Plugin_Interface
 {
     /**
      * 激活插件方法,如果激活失败,直接抛出异常
-     * 
+     *
      * @access public
      * @return void
      * @throws Typecho_Plugin_Exception
@@ -21,15 +21,19 @@ class majors_Plugin implements Typecho_Plugin_Interface
 
 
         //Helper::addPanel(4, 'majors/manage.php', 'MAJOR 配置', 'MAJOR 主题配置', 'administrator')
-			
-		//添加文章
+
+        Typecho_Plugin::factory('Widget_Abstract_Contents')->markdown = array('majors_Plugin', 'markdown');
+        Typecho_Plugin::factory('Widget_Abstract_Comments')->markdown = array('majors_Plugin', 'markdown');
+
+
+        //添加文章
         Typecho_Plugin::factory('admin/write-post.php')->option = array('majors_Plugin', 'formatsSelect');
 
         //添加views
         Typecho_Plugin::factory('Widget_Archive')->beforeRender = array('majors_Plugin', 'viewsCounter');
-		
-		//编辑文章
-		Typecho_Plugin::factory('Widget_Contents_Post_Edit')->write = array('majors_Plugin', 'formatsSet');
+
+        //编辑文章
+        Typecho_Plugin::factory('Widget_Contents_Post_Edit')->write = array('majors_Plugin', 'formatsSet');
         Typecho_Plugin::factory('Widget_Archive')->indexHandle = array('majors_Plugin', 'sticky');
         Typecho_Plugin::factory('Widget_Archive')->categoryHandle = array('majors_Plugin', 'sticky');
 
@@ -41,7 +45,7 @@ class majors_Plugin implements Typecho_Plugin_Interface
 
     /**
      * 禁用插件方法,如果禁用失败,直接抛出异常
-     * 
+     *
      * @static
      * @access public
      * @return void
@@ -61,10 +65,10 @@ class majors_Plugin implements Typecho_Plugin_Interface
             }
         }
     }
-    
+
     /**
      * 获取插件配置面板
-     * 
+     *
      * @access public
      * @param Typecho_Widget_Helper_Form $form 配置面板
      * @return void
@@ -78,11 +82,11 @@ class majors_Plugin implements Typecho_Plugin_Interface
         ), '1', '删除数据表:', '请选择是否在禁用插件时，format字段');
         $form->addInput($delmat);
 
-	}
+    }
 
     /**
      * 个人用户的配置面板
-     * 
+     *
      * @access public
      * @param Typecho_Widget_Helper_Form $form
      * @return void
@@ -96,12 +100,12 @@ class majors_Plugin implements Typecho_Plugin_Interface
 
         $db = Typecho_Db::get();
         $prefix = $db->getPrefix();
-        
+
         // contents 表中若无 views 字段则添加
         if (!array_key_exists('views', $db->fetchRow($db->select()->from('table.contents')))){
             $db->query('ALTER TABLE `'. $prefix .'contents` ADD `views` INT(10) DEFAULT 0;');
         }
-            
+
         // contents 表中若无 format 字段则添加
         if (!array_key_exists('format', $db->fetchRow($db->select()->from('table.contents')))) {
             $db->query("ALTER TABLE `".$prefix."contents` ADD `format` varchar(16) DEFAULT 'post'");
@@ -109,6 +113,13 @@ class majors_Plugin implements Typecho_Plugin_Interface
         } else {
             return 'format 字段已存在，插件启用成功，' . $configLink;
         }
+    }
+
+    public static function markdown($text)
+    {
+        require_once dirname(__FILE__) . '/HyperDown.php';
+        $HyperDown = new HyperDown();
+        return $HyperDown->makeHtml($text);
     }
 
 
@@ -121,108 +132,108 @@ class majors_Plugin implements Typecho_Plugin_Interface
      */
     public static function formatsSelect()
     {
-		$custom_format =  Helper::options()->format;
-		$regular_format = array(
-            'post'=>'标准', 
-            'aside'=>'日志', 
-            'gallery'=>'相册', 
-            'link'=>'链接', 
-            'quote'=>'引语', 
-            'status'=>'状态', 
-            'video'=>'视频', 
-            'audio'=>'音频', 
+        $custom_format =  Helper::options()->format;
+        $regular_format = array(
+            'post'=>'标准',
+            'aside'=>'日志',
+            'gallery'=>'相册',
+            'link'=>'链接',
+            'quote'=>'引语',
+            'status'=>'状态',
+            'video'=>'视频',
+            'audio'=>'音频',
             'chat'=>'聊天');
-		if(count($custom_format)>0){
-			$args = array('post'=>'标准');
-			foreach($regular_format as $key=>$val){
-				if(in_array($key,$custom_format)){
-					$args[$key] = $val;
-				}
-			}
-		}else{
-			$args = array(
-                'post'=>'标准', 
-                'aside'=>'日志', 
-                'gallery'=>'相册', 
-                'link'=>'链接', 
-                'quote'=>'引语', 
-                'status'=>'状态', 
-                'video'=>'视频', 
-                'audio'=>'音频', 
+        if(count($custom_format)>0){
+            $args = array('post'=>'标准');
+            foreach($regular_format as $key=>$val){
+                if(in_array($key,$custom_format)){
+                    $args[$key] = $val;
+                }
+            }
+        }else{
+            $args = array(
+                'post'=>'标准',
+                'aside'=>'日志',
+                'gallery'=>'相册',
+                'link'=>'链接',
+                'quote'=>'引语',
+                'status'=>'状态',
+                'video'=>'视频',
+                'audio'=>'音频',
                 'chat'=>'聊天');
-		}
-		if(isset($_GET['cid'])){
-			$cid = $_GET['cid'];
-			$db = Typecho_Db::get();
-			$row = $db->fetchRow($db->select('format')->from('table.contents')->where('cid = ?', $cid));
-			$format = $row['format'];
-		}else{
-			$format = "post";
-		}
-		$output = '';
-		foreach( $args as $key => $value ){
-			$check = $key == $format ? 'checked="checked"' :'';
-			$output .= '<li><input type="radio" name="format" id="format-'.$key.'" value="'.$key.'" '.$check.'><label for="format-'.$key.'">'.$value.'</label></li>';
-		}
-		echo '<li><label class="typecho-label">形式</label><ul>'.$output.'</ul></li>';
+        }
+        if(isset($_GET['cid'])){
+            $cid = $_GET['cid'];
+            $db = Typecho_Db::get();
+            $row = $db->fetchRow($db->select('format')->from('table.contents')->where('cid = ?', $cid));
+            $format = $row['format'];
+        }else{
+            $format = "post";
+        }
+        $output = '';
+        foreach( $args as $key => $value ){
+            $check = $key == $format ? 'checked="checked"' :'';
+            $output .= '<li><input type="radio" name="format" id="format-'.$key.'" value="'.$key.'" '.$check.'><label for="format-'.$key.'">'.$value.'</label></li>';
+        }
+        echo '<li><label class="typecho-label">形式</label><ul>'.$output.'</ul></li>';
     }
-	
+
     /**
      * 拓展开发 提交文章, 设置文章形式
      *
-     * 
+     *
      * @return void
      */
-	public static function formatsSet($contents, $inst)
-	{
-		$db = Typecho_Db::get();
-		Typecho_Widget::widget('Widget_Contents_Post_Edit')->to($post);	
-		$cid = $post->cid;
-		if($cid!=null){ // 文章已存在, 直接修改 format 字段
-			$db->query($db->update('table.contents')->rows(array('format' => $_POST['format']))->where('cid = ?', $cid));
-			return $contents;
-		}else{ // 文章不存在, 新建文章
-			$options = Typecho_Widget::widget('Widget_Options');
-			
-			if( $contents['title']!="" ){
-				$contents['status'] = $_POST['do']== 'publish' ? 'publish':'draft';
-				$cid = $post->insert($contents);
+    public static function formatsSet($contents, $inst)
+    {
+        $db = Typecho_Db::get();
+        Typecho_Widget::widget('Widget_Contents_Post_Edit')->to($post);
+        $cid = $post->cid;
+        if($cid!=null){ // 文章已存在, 直接修改 format 字段
+            $db->query($db->update('table.contents')->rows(array('format' => $_POST['format']))->where('cid = ?', $cid));
+            return $contents;
+        }else{ // 文章不存在, 新建文章
+            $options = Typecho_Widget::widget('Widget_Options');
 
-				/** 插入分类 */
-				if (array_key_exists('category', $contents)) {
-					$post->setCategories($cid, !empty($contents['category']) && is_array($contents['category']) ?
-					$contents['category'] : array($options->defaultCategory), false, true);
-				}
+            if( $contents['title']!="" ){
+                $contents['status'] = $_POST['do']== 'publish' ? 'publish':'draft';
+                $cid = $post->insert($contents);
 
-				/** 插入标签 */
-				if (array_key_exists('tags', $contents)) {
-					$post->setTags($cid, $contents['tags'], false, true);
-				}
+                /** 插入分类 */
+                if (array_key_exists('category', $contents)) {
+                    $post->setCategories($cid, !empty($contents['category']) && is_array($contents['category']) ?
+                        $contents['category'] : array($options->defaultCategory), false, true);
+                }
 
-				/** 同步附件 */
-				$post->attach($cid);
-				
-				/** 文章形式 */
-				$db->query($db->update('table.contents')->rows(array('format' => $_POST['format']))->where('cid = ?', $cid));
-				
-				$post->fetchRow($post->select()->where('table.contents.cid = ?', $cid)->limit(1), array($post, 'push'));
-				
-				/** 新建提示 */
-				$newPost = $db->fetchRow($post->select()->where('table.contents.cid = ?', $cid)->limit(1));
-				$result = Typecho_Widget::widget('Widget_Abstract_Contents')->push($newPost);
-				$post->widget('Widget_Notice')->set('publish' == $result['status'] ?
-				_t('文章 "<a href="%s">%s</a>" 已经发布', $result['permalink'], $result['title']) :
-				_t('文章 "%s" 等待审核', $result['title']), NULL, 'success');
+                /** 插入标签 */
+                if (array_key_exists('tags', $contents)) {
+                    $post->setTags($cid, $contents['tags'], false, true);
+                }
 
-				/** 设置高亮 */
-				$post->widget('Widget_Notice')->highlight($cid);
-			}
-			$post->response->redirect(Typecho_Common::url('manage-posts.php', $options->adminUrl));
-			exit;
-		}
-	}
+                /** 同步附件 */
+                $post->attach($cid);
 
-	/**
+                /** 文章形式 */
+                $db->query($db->update('table.contents')->rows(array('format' => $_POST['format']))->where('cid = ?', $cid));
+
+                $post->fetchRow($post->select()->where('table.contents.cid = ?', $cid)->limit(1), array($post, 'push'));
+
+                /** 新建提示 */
+                $newPost = $db->fetchRow($post->select()->where('table.contents.cid = ?', $cid)->limit(1));
+                $result = Typecho_Widget::widget('Widget_Abstract_Contents')->push($newPost);
+                $post->widget('Widget_Notice')->set('publish' == $result['status'] ?
+                    _t('文章 "<a href="%s">%s</a>" 已经发布', $result['permalink'], $result['title']) :
+                    _t('文章 "%s" 等待审核', $result['title']), NULL, 'success');
+
+                /** 设置高亮 */
+                $post->widget('Widget_Notice')->highlight($cid);
+            }
+            $post->response->redirect(Typecho_Common::url('manage-posts.php', $options->adminUrl));
+            exit;
+        }
+    }
+
+    /**
      * 拓展开发 输出文章形式
      *
      * 语法: majors_Plugin::getFormat();
@@ -230,18 +241,18 @@ class majors_Plugin implements Typecho_Plugin_Interface
      * @access public
      * @return void
      */
-	public static function getFormat()
-	{
+    public static function getFormat()
+    {
         $db = Typecho_Db::get();
         $cid = Typecho_Widget::widget('Widget_Archive')->cid;
         $row = $db->fetchRow($db->select('format')->from('table.contents')->where('cid = ?', $cid));
-		return $row['format'];
-	}
-    
-    
+        return $row['format'];
+    }
+
+
     /**
      * 拓展开发 加入 beforeRender
-     * 
+     *
      * @access public
      * @return void
      */
@@ -282,27 +293,27 @@ class majors_Plugin implements Typecho_Plugin_Interface
             return $row['views'];
         }
     }
-  
-     /**
+
+    /**
      * 拓展开发 输出浏览总数
      *
      *
      * @access public
      * @return string
      */
-    
+
     public static function sumViews()
     {
-      $db = Typecho_Db::get();
-      $query= $db->select('sum(views) as num')->from('table.contents');
-      $result = $db->fetchAll($query);
-      echo $result[0]['num'];
-      
+        $db = Typecho_Db::get();
+        $query= $db->select('sum(views) as num')->from('table.contents');
+        $result = $db->fetchAll($query);
+        echo $result[0]['num'];
+
     }
-    
+
     /**
      * 拓展开发 选取置顶文章
-     * 
+     *
      * @access public
      * @param object $archive, $select
      * @return void
@@ -318,13 +329,13 @@ class majors_Plugin implements Typecho_Plugin_Interface
         $sticky_html = "<strong>[置顶]</strong>";
 
         foreach($sticky_cids as $cid) {
-          if ($cid && $sticky_post = $db->fetchRow($archive->select()->where('cid = ?', $cid))) {
-              if ($paded == 1) {                               // 首頁 page.1 才會有置頂文章
-                $sticky_post['sticky'] = $sticky_html;
-                $archive->push($sticky_post);                  // 選取置頂的文章先壓入
-              }
-              $select->where('table.contents.cid != ?', $cid); // 使文章不重覆
-          }
+            if ($cid && $sticky_post = $db->fetchRow($archive->select()->where('cid = ?', $cid))) {
+                if ($paded == 1) {                               // 首頁 page.1 才會有置頂文章
+                    $sticky_post['sticky'] = $sticky_html;
+                    $archive->push($sticky_post);                  // 選取置頂的文章先壓入
+                }
+                $select->where('table.contents.cid != ?', $cid); // 使文章不重覆
+            }
         }
     }
 
@@ -360,5 +371,5 @@ class majors_Plugin implements Typecho_Plugin_Interface
         return $url;
     }
 
-    
+
 }
